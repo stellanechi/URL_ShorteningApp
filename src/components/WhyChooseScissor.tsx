22b26e55879ce065cedcba2206250adca3074ce4
stellanechi <stellanechi65@gmail.com>
2024-02-16 11:05:35 +0100

    fix

--- src/components/WhyChooseScissor.tsx
+++ src/components/WhyChooseScissor.tsx
@@ -2,19 +2,45 @@ import React from "react";
 
 function WhyChooseScissor() {
   return (
-    <div className="w-100 h-20 bg-gray-700">
+    <div className="flex w-100 h-20 bg-gray-700 text-white p-5">
       <div>
         {" "}
-        <h1 className="text-white">
+        <h1>
           {" "}
           One Stop. <br /> Four
           <span className="text-blue-500 "> Possibilities. </span>{" "}
         </h1>
       </div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div>
+        {" "}
+        <h1>
+          {" "}
+          30M <br />
+          Active Users{" "}
+        </h1>
+      </div>
+      <div>
+        {" "}
+        <h1>
+          60M <br /> Links & QR <br /> codes created{" "}
+        </h1>
+      </div>
+      <div>
+        {" "}
+        <h1>
+          {" "}
+          1B <br />
+          Clicked & scanned <br />
+          connections
+        </h1>
+      </div>
+      <div>
+        {" "}
+        <h1>
+          {" "}
+          300K <br /> App integrations{" "}
+        </h1>
+      </div>
     </div>
   );
 }
