653dda39d83b37cc4fd70f429a56f9d7dc4b6c45
stellanechi <stellanechi65@gmail.com>
2024-02-19 16:39:47 +0100

    updated

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -40,13 +40,50 @@ function HeroSection() {
         </span>
       </article>
       {/* SHORTEN LINK SECTION */}
-      <div className="flex justify-center items-center mb-40">
+      <div className="flex justify-center items-center mb-10">
         <button className={HeroCSS.urlbtn}>
           {" "}
           <FaLink className={HeroCSS.urlicon} />
           <p className={HeroCSS.urltext}> Enter the link here </p>{" "}
           <button className={HeroCSS.innerbtn}> Shorten now </button>{" "}
         </button>
+      </div>
+      {/* TABLE SECTION  TAILWIND CSS STYLING*/}
+      <div className="flex justify-center">
+        <table className=" w-20 text-slate-200 border-seperate p-8 border-5 border-solid bg-gray-900 mb-10">
+          <tr>
+            <th className=" bg-gray-700 text-left px-8 py-4">Short Link </th>
+            <th className=" bg-gray-700 text-left px-8 py-4">Original Link </th>
+            <th className=" bg-gray-700 text-left px-8 py-4"> QR Code </th>
+            <th className="bg-gray-700 text-left px-8 py-4"> Clicks</th>
+            <th className="bg-gray-700 text-left px-8 py-4">Status</th>
+            <th className=" bg-gray-700 text-left px-8 py-4">Date </th>
+          </tr>
+          <tr className="text-left px-8 py-4">
+            <td>Alfreds Futterkiste</td>
+            <td>Dante Sparks</td>
+            <td>Italy</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Dante Sparks</td>
+            <td>Italy</td>
+          </tr>
+          <tr className="text-left px-8 py-4">
+            <td>Alfreds Futterkiste</td>
+            <td>Dante Sparks</td>
+            <td>Italy</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Dante Sparks</td>
+            <td>Italy</td>
+          </tr>
+          <tr className="text-left px-8 py-4">
+            <td>Alfreds Futterkiste</td>
+            <td>Dante Sparks</td>
+            <td>Italy</td>
+            <td>Alfreds Futterkiste</td>
+            <td>Dante Sparks</td>
+            <td>Italy</td>
+          </tr>
+        </table>
       </div>
     </>
   );
