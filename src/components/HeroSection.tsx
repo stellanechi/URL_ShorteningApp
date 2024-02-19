e7ba9acfb2acab95bf680c2eeaeb041b885403de
stellanechi <stellanechi65@gmail.com>
2024-02-19 17:26:33 +0100

    updated qr section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,6 +2,7 @@ import HeroCSS from "./HeroSection.module.css";
 // import cube from "../assets/cube.svg";
 import { FiLogOut } from "react-icons/fi";
 import { FaLink } from "react-icons/fa";
+import { IoQrCodeOutline } from "react-icons/io5";
 
 function HeroSection() {
   return (
@@ -50,8 +51,8 @@ function HeroSection() {
       </div>
       {/* TABLE SECTION  TAILWIND CSS STYLING*/}
       <div className="flex justify-center">
-        <table className=" w-20 text-slate-200 border-seperate p-8 border-5 border-solid bg-gray-900 mb-10">
-          <tr>
+        <table className=" text-slate-200 border-seperate p-8 border-5 border-solid bg-gray-900 mb-10">
+          <tr className="">
             <th className=" bg-gray-700 text-left px-8 py-4">Short Link </th>
             <th className=" bg-gray-700 text-left px-8 py-4">Original Link </th>
             <th className=" bg-gray-700 text-left px-8 py-4"> QR Code </th>
@@ -59,29 +60,36 @@ function HeroSection() {
             <th className="bg-gray-700 text-left px-8 py-4">Status</th>
             <th className=" bg-gray-700 text-left px-8 py-4">Date </th>
           </tr>
-          <tr className="text-left px-8 py-4">
-            <td>Alfreds Futterkiste</td>
-            <td>Dante Sparks</td>
-            <td>Italy</td>
-            <td>Alfreds Futterkiste</td>
-            <td>Dante Sparks</td>
-            <td>Italy</td>
+          <tr>
+            <td className="text-left px-8 py-4">
+              https://blog.logrocket.com/building
+            </td>
+            <td className="text-left px-8 py-4">
+              https://blog.logrocket.com/building-tailwind-cs
+            </td>
+            <td className="text-left px-8 py-4">
+              {" "}
+              <IoQrCodeOutline className=" text-slate -200 text-3xl " />
+            </td>
+            <td className="text-left px-8 py-4"> 1313</td>
+            <td className="text-left px-8 py-4 text-emerald-400 ">Active </td>
+            <td className="text-left px-8 py-4">02-19-2024</td>
           </tr>
-          <tr className="text-left px-8 py-4">
-            <td>Alfreds Futterkiste</td>
-            <td>Dante Sparks</td>
-            <td>Italy</td>
-            <td>Alfreds Futterkiste</td>
-            <td>Dante Sparks</td>
-            <td>Italy</td>
-          </tr>
-          <tr className="text-left px-8 py-4">
-            <td>Alfreds Futterkiste</td>
-            <td>Dante Sparks</td>
-            <td>Italy</td>
-            <td>Alfreds Futterkiste</td>
-            <td>Dante Sparks</td>
-            <td>Italy</td>
+
+          <tr>
+            <td className="text-left px-8 py-4">
+              https://blog.logrocket.com/building
+            </td>
+            <td className="text-left px-8 py-4">
+              https://blog.logrocket.com/building-tailwind-cs
+            </td>
+            <td className="text-left px-8 py-4">
+              {" "}
+              <IoQrCodeOutline className=" text-slate-200 text-3xl " />
+            </td>
+            <td className="text-left px-8 py-4"> 4393</td>
+            <td className="text-left px-8 py-4 text-pink-500">inactive </td>
+            <td className="text-left px-8 py-4">02-16-2024</td>
           </tr>
         </table>
       </div>
