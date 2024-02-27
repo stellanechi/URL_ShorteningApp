2712db9f77e944d88884d62f7f6268c80073ba1d
stellanechi <stellanechi65@gmail.com>
2024-02-27 16:38:49 +0100

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,36 +11,81 @@ import {
 function Footer() {
   return (
     <>
-      <div className="bg-slate-200">
-        <div>
-          <div>
-            <div className="flex font-black mb-5">
-              <span className="text-3xl font-semibold"> Linkly </span>{" "}
+      <footer>
+        <div className="bg-slate-200 ">
+          <div className="flex gap-16 justify-center">
+            <div>
+              <div className="flex font-black mb-5">
+                <h1 className="text-3xl font-black"> Linkly </h1>{" "}
+              </div>
+              {/* END OF LINKLY */}
+              <div className="flex  gap-4">
+                <span>
+                  {" "}
+                  <FaTwitter />
+                </span>
+                <span>
+                  {" "}
+                  <BsInstagram />
+                </span>
+                <span>
+                  <FaLinkedin />
+                </span>
+                <span>
+                  <FaSquareFacebook />
+                </span>
+              </div>
             </div>
-            {/* END OF LINKLY */}
-            <div className="flex  gap-0.5">
-              <span>
-                {" "}
-                <FaTwitter />
-              </span>
-              <span>
-                {" "}
-                <BsInstagram />
-              </span>
-              <span>
-                <FaLinkedin />
-              </span>
-              <span>
-                <FaSquareFacebook />
-              </span>
+            {/* START OF FLEX ELEMENT */}
+            <div className="flex  gap-16 ">
+              {/* ONE */}
+              <div>
+                <h1 className="text-xl font-medium mb-4"> Why Linkly </h1>
+                <p>
+                  {" "}
+                  Integrations & Api <br />
+                  Pricing
+                </p>
+              </div>
+              {/* TWO */}
+              <div>
+                <h1 className="text-xl font-medium mb-4"> Solutions </h1>
+                <p>
+                  {" "}
+                  Social Media <br />
+                  Digital Marketing <br />
+                  Customer Service <br />
+                  For Developers
+                </p>
+              </div>
+              {/* THREE */}
+              <div>
+                <h1 className="text-xl font-medium mb-4"> Products </h1>
+                <p>
+                  {" "}
+                  Link management
+                  <br />
+                  QR Code <br />
+                  Link-in-bio
+                </p>
+              </div>
+              {/* FOUR */}
+              <div>
+                <h1 className="text-xl font-medium mb-4 "> Company</h1>
+                <p>
+                  {" "}
+                  About Linkly <br />
+                  Carrers <br />
+                  Partners <br />
+                  Press <br />
+                  Contact <br />
+                  Reviews
+                </p>
+              </div>
             </div>
           </div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
         </div>
-      </div>
+      </footer>
     </>
   );
 }
