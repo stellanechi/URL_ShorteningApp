fe09c681caf75e71cdccbbda7a6aed33c238fd92
stellanechi <stellanechi65@gmail.com>
2024-03-01 22:11:05 +0100

    updated

--- src/pages/FAQ.tsx
+++ src/pages/FAQ.tsx
@@ -1,7 +1,16 @@
 // import React from 'react'
 
 function FAQ() {
-  return <div>FAQ</div>;
+  return (
+    <>
+      <div>
+        <div className=" flex justify-center text-slate-200 ">
+          {" "}
+          <h1>FAQ </h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default FAQ;
