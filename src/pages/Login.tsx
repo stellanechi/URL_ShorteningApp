0d73fc1b373c3b422c0143422f026d1b9a974aea
stellanechi <stellanechi65@gmail.com>
2024-03-03 15:27:12 +0100

    fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -8,7 +8,9 @@ const Login = () => {
   return (
     <div className=" flex items-center justify-center">
       <div className="bg-white p-20 rounded shadow-md w-2/5">
-        <h2 className="flex justify-center text-3xl font-black mb-6 ">Login</h2>
+        <h2 className="flex justify-center text-3xl font-black mb-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+          Login
+        </h2>
         <form>
           <div className="mb-4">
             <label htmlFor="username" className="block text-gray-600">
@@ -35,10 +37,17 @@ const Login = () => {
 
           <button
             type="submit"
-            className="flex justify-center bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            className="px-6 py-2 rounded font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 hover:from-pink-500 hover:to-purple-500 ..."
           >
             Login
           </button>
+
+          {/* <button
+            type="submit"
+            className="flex justify-center bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600"
+          >
+            Login
+          </button> */}
         </form>
       </div>
     </div>
