9c0d8f474fad19f20ab843c8644aa4fa28eecc16
stellanechi <stellanechi65@gmail.com>
2024-03-05 08:45:59 +0100

    fix

---
+++ src/firebase/Firebase.tsx
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDk_EqX91LkGp2LKlenLLfV10TdNVDMHu4",
+  authDomain: "auth-d5b32.firebaseapp.com",
+  projectId: "auth-d5b32",
+  storageBucket: "auth-d5b32.appspot.com",
+  messagingSenderId: "892742725368",
+  appId: "1:892742725368:web:6f0b2986cf4522dbb4550d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { app, auth };
