b3404114df935531646e2456246621570926019d
stellanechi <stellanechi65@gmail.com>
2024-03-05 11:20:38 +0100

    added auth

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,48 @@
-// import React from "react";
-// import loginstyle from "../pages/login.module.css";
-
-// src/Login.js
-// import React from 'react';
+import React, { useState } from "react";
+import { Auth, signInWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className=" flex items-center justify-center">
       <div className="bg-white p-20 rounded shadow-md w-2/5">
         <h2 className="flex justify-center text-3xl font-black mb-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
           Welcome Back
         </h2>
-        <form>
+        <form onSubmit={signIn}>
           <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-600">
-              Username:
+            <label htmlFor="email" className="block text-gray-600">
+              Enter Email:
             </label>
             <input
-              type="text"
-              id="username"
-              name="username"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              name="email"
               className="w-full border border-gray-300 p-2 rounded"
             />
           </div>
           <div className="mb-4">
             <label htmlFor="password" className="block text-gray-600">
-              Password:
+              Enter Password:
             </label>
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               id="password"
               name="password"
               className="w-full border border-gray-300 p-2 rounded"
