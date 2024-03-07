21353c3b27e0cdb82e11cec91bf499cc68c71dc0
stellanechi <stellanechi65@gmail.com>
2024-03-07 16:22:13 +0100

    changed font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ function Header() {
       {" "}
       <nav className="flex justify-between p-6 items-center  bg-gray-900 fixed top-0 w-full z-50">
         <Link
-          className=" text-lg md:text-4xl font-bold md:font-black bg-gradient-to-r from-pink-500 via-indigo-500 to-pink-500 text-transparent bg-clip-text"
+          className=" text-2xl md:text-4xl font-bold md:font-black bg-gradient-to-r from-pink-500 via-indigo-500 to-pink-500 text-transparent bg-clip-text"
           to="/"
         >
           {" "}
@@ -40,7 +40,7 @@ function Header() {
           {nav ? (
             <FaTimes className="text-slate-200" size={25} />
           ) : (
-            <GiHamburgerMenu className=" p-4 bg-slate-200 " size={25} />
+            <GiHamburgerMenu className=" text-slate-200 " size={25} />
           )}
           {/*  */}
         </div>
