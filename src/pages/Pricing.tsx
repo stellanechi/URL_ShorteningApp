78cb1a0b6cba8eec1c1cc7dc1b293f17681603bd
stellanechi <stellanechi65@gmail.com>
2024-03-11 11:09:49 +0100

    added price page

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,7 +1,25 @@
 // import React from 'react'
 
 function Pricing() {
-  return <div>Pricing</div>;
+  return (
+    <>
+      <div className=" flex justify-center items-center bg-slate-200">
+        <div className="">
+          <h1 className="text-xl md:text-3xl font-semibold text-center">
+            {" "}
+            A <span className="text-blue-700"> price perfect </span> for your
+            needs.
+          </h1>
+          <p className="mt-2 md:mt-3 text-center">
+            {" "}
+            From catering for your personal, business, event and social needs,
+            you can be <br />
+            rest assured we have you in mind in our pricing.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Pricing;
