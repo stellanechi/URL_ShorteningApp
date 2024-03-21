bf5a24d936fb40fe1170959ff4334af889304338
stellanechi <stellanechi65@gmail.com>
2024-03-21 10:12:36 +0100

    error fixed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -45,10 +45,23 @@ function Home() {
       setShortenedUrl(response.data.id);
       setShowQRCode(true);
     } catch (error) {
-      // } catch (typeof error === 'string') {
       console.error("Error shortening URL:", error.response.data);
       setErrorMessage("Error shortening URL. Please try again.");
     }
+    // ERROR TRIAL
+    // } catch (error) {
+    //   if (typeof error === "string") {
+    //     console.error("Error shortening URL:", error);
+    //     setErrorMessage("Error shortening URL. Please try again.");
+    //   } else if (error.response && error.response.data) {
+    //     console.error("Error shortening URL:", error!.response.data);
+    //     setErrorMessage("Error shortening URL. Please try again.");
+    //   } else {
+    //     console.error("Unknown error occurred:", error);
+    //     setErrorMessage("Unknown error occurred. Please try again.");
+    //   }
+    // }
+    // END OF ERROR TRIAL
   };
   // END OF URL SHORTENING LOGIC
   return (
